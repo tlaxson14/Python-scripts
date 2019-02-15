d5310bda314e6a1afe809ad06a0044f2e0f569b9
Travis Laxson <laxsont@oregonstate.edu>
2019-02-15 13:22:40 -0800

    Initial commit

---
+++ mypython.py
@@ -0,0 +1,24 @@
+#######################################################
+# Name: Travis Laxson
+# Date: 15 Feb 2019
+# ONID: laxsont
+# Email: laxsont@oregonstate.edu
+# Description: Python script file that
+# performs the follwing actions:
+# 	1) Create 3 files in directory
+#	2) Each file contains 10 random chars from
+#	   the lowercase alphabet, no spaces
+#	3) Final, eleventh char of each file is
+#	   a new line character
+#	4) Generate random integer values and 
+#  	   perform basic arithmetic on them
+# 
+# Upon execution the script will first print 
+# out the contents of the 3 created files. Then
+# the values of the two randomly generated numbers 
+# separated by newlines will display. Lastly, the 
+# script will calculate and display the product of
+# the two randomly generated integer values on the
+# final line of output.   
+#
+######################################################
